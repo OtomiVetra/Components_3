7b82461544097fd22e3271ee624143c3f196bd1e
Alexey <otomivetra@mail.ru>
2024-06-09 13:36:57 +0300

    add calculation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,62 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
+import style from './App.module.css';
+import React, { useState } from 'react';
 
+const signs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '+', '=', 'C'];
 export const App = () => {
-	const currentDate = new Date().toLocaleDateString();
-	return React.createElement(
-		"div",
-		{
-			className: "App",
-		},
-		React.createElement(
-			"header",
-			{
-				className: "App-header",
-			},
-			React.createElement("img", {
-				src: logo,
-				className: "App-logo",
-				alt: "logo",
-			}),
-			React.createElement(
-				"p",
-				null,
-				"Edit ",
-				React.createElement("code", null, "src/App.js"),
-				" and save to reload fuck.",
-			),
-			React.createElement(
-				"a",
-				{
-					className: "App-link",
-					href: "https://reactjs.org",
-					target: "_blank",
-					rel: "noopener noreferrer",
-				},
-				"Learn React",
-				React.createElement("p", null, currentDate),
-			),
-		),
+	const [operator, setOperator] = useState('');
+	const [operand1, setOperand1] = useState('');
+	const [operand2, setOperand2] = useState('');
+	const [result, setResult] = useState(null);
+
+	const handleReset = () => {
+		setOperand1('');
+		setOperand2('');
+		setOperator('');
+		setResult(null);
+	};
+
+	const mathOperation = () => {
+		switch (operator) {
+			case '+':
+				return setResult(Number(operand1) + Number(operand2));
+			case '-':
+				return setResult(Number(operand1) - Number(operand2));
+		}
+	};
+
+	const handleClick = (num) => {
+		if (operand1 && operand2 && operator && num === '=') {
+			setOperand2('');
+			mathOperation();
+		}
+		if (result) {
+			setOperand1(result);
+			setResult(null);
+		}
+		if (num === 'C') return handleReset();
+		const isNum = !isNaN(num);
+		if (operand1 && !isNum) {
+			setOperator(num);
+			return;
+		}
+		if (!operator && isNum) {
+			setOperand1((prev) => prev + num);
+		} else if (operator && isNum) {
+			setOperand2((prev) => prev + num);
+		}
+	};
+
+	const res = result || operand1 + operator + operand2;
+	return (
+		<div className={style.wrapper}>
+			<input className={style.textField + ' ' + (result && style.active) } disabled type='text' defaultValue={res} />
+			<div className={style.calculator}>
+				{signs.map((sign, index) => (
+					<button onClick={() => handleClick(sign)} key={index}>
+						{sign}
+					</button>
+				))}
+			</div>
+		</div>
 	);
 };
